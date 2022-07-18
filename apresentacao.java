250f01168ea7cde08d4611fc4a6e932579c33275
Beatriz Nataly <84404795+BeatrizNataly@users.noreply.github.com>
2022-07-18 17:18:05 -0300

    Create apresentacao.java

---
+++ apresentacao.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    /*
+    * ATENÇÃO: Este arquivo Java se destina a apresentação resumida da aplicação e NÃO deve ser acrescentado à pasta/zip do projeto baixado ao executá-lo.
+    O projeto baixado em .zip se trata de um projeto Android Studio realizado utilizando a linguagem de programação Java e a versão 2021.1.1 (Bumblebee) do Android Studio.
+    */
+    
+    System.out.println("Olá mundo!");
+    System.out.println("Muito obrigada por visitar meu projeto. \n" +
+                      "Para testá-lo por favor baixe o .zip mais recente na pasta main. Preferencialmente alguma versão fechada: " 
+                       + " -- que NÃO possua 'beta' no nome --\n" +
+                      "Extraia o zip na pasta de sua preferência e rode o projeto utilizando Android Studio. Divirta-se!");
+    System.out.println("\nPara relatar erros, problemas ou sugerir melhorias, por favor visite: https://github.com/BeatrizNataly/pokedex/issues/");
+    System.out.println("\nNão se esqueça de ler o README.md na página principal do projeto para mais informações relevantes.");
+    System.out.println("\n\n ==> Projeto desenvolvido por Beatriz Nataly Silva em 2022.");
+  }
+}
